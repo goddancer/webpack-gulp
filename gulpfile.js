561437f755821519e486a3287a765da7633a67f9
goddancer <goddancer@foxmail.com>
2018-06-04 18:20:38 +0800

    Autoprefixer remove old css name such as -webkit-box-orient

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulpTask = require('./.gulplock');
 gulpTask({
   taskName: 'default',
-  taskEntry: './test/demo/js/main.js',
-  taskDist: './test/demo/dist/'
+  taskWatch: true,
+  inject: false,
+  taskHtmlEntry: './test/demo/index.html',
+  taskJsEntry: './test/demo/js/main.js',
+  taskCssEntry: './test/demo/css/',
+  taskDist: './test/demo/dist/',
 });
